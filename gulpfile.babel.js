63c4f98fdd8147266ff081a83bf53334a16d4519
jiee <jee.vvvv@gmail.com>
2022-01-08 15:13:50 +0900

    gulp-pug, del

---
+++ gulpfile.babel.js
@@ -0,0 +1,24 @@
+import gulp from "gulp";
+import gpug from "gulp-pug";
+import del from "del";
+
+const routes = {
+	pug: {
+		src: "src/*.pug",
+		dest: "build"
+	}
+}
+
+const pug = () => 
+	gulp
+		.src(routes.pug.src)
+		.pipe(gpug())
+		.pipe(gulp.dest(routes.pug.dest));
+
+const clean = () => del(["build"]);
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+export const dev = gulp.series([prepare, assets])
