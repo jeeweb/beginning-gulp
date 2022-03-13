d463d3ceb25256a978869fe1bbfee687674d2439
jeeweb <52396673+jeeweb@users.noreply.github.com>
2022-03-13 16:46:53 +0900

    ghPages 배포

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,6 +8,7 @@ import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
 import bro from "gulp-bro";
 import babelify from "babelify";
+import ghPages from "gulp-gh-pages";
 
 const sass = require("gulp-sass")(require("node-sass"));
 
@@ -37,7 +38,7 @@ const routes = {
 
 const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]);
+const clean = () => del(["build/", ".publish"]);
 
 const webserver = () => gulp.src("build").pipe(ws({ livereload: true, open: true }))
 
@@ -65,6 +66,8 @@ const js = () =>
         }))
         .pipe(gulp.dest(routes.js.dest));
 
+const gh = () => gulp.src("build/**/*").pipe(ghPages());
+
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
     gulp.watch(routes.img.src, img);
@@ -73,9 +76,9 @@ const watch = () => {
 }
 
 const prepare = gulp.series([clean, img]);
-
 const assets = gulp.series([pug, styles, js]);
-
 const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, live]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, live]);
+export const deploy = gulp.series([build, gh, clean])
