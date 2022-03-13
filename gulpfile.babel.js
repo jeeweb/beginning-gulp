1878499e8d5e7ba165f585494ca8c0b3a271c080
jeeweb <52396673+jeeweb@users.noreply.github.com>
2022-03-13 16:07:40 +0900

    js, babelify 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,4 @@
-import gulp from "gulp";
+import gulp, { dest } from "gulp";
 // const gulp = require("gulp");
 import gpug from "gulp-pug";
 import del from "del";
@@ -6,6 +6,8 @@ import ws from "gulp-webserver";
 import image from "gulp-imagemin";
 import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
+import bro from "gulp-bro";
+import babelify from "babelify";
 
 const sass = require("gulp-sass")(require("node-sass"));
 
@@ -23,6 +25,11 @@ const routes = {
         watch: "src/scss/**/*.scss",
         src: "src/scss/style.scss",
         dest: "build/css"
+    },
+    js: {
+        watch: "src/js/**/*.js",
+        src: "src/js/main.js",
+        dest: "build/js"
     }
 }
 
@@ -48,15 +55,26 @@ const styles = () =>
         .pipe(miniCSS())
         .pipe(gulp.dest(routes.scss.dest));
 
+const js = () => 
+    gulp.src(routes.js.src)
+        .pipe(bro({
+            transform: [
+                babelify.configure({ presets: ["@babel/preset-env"]}),
+                [ "uglifyify", { global: true }]
+            ]
+        }))
+        .pipe(gulp.dest(routes.js.dest));
+
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
     gulp.watch(routes.img.src, img);
     gulp.watch(routes.scss.watch, styles);
+    gulp.watch(routes.js.watch, js);
 }
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug, styles]);
+const assets = gulp.series([pug, styles, js]);
 
 const live = gulp.parallel([webserver, watch]);
 
